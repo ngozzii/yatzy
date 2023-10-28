582ee149cfbb2c70267a9d73f0009ff6b8105ab9
ngozzii <isiu0001@algonquinlive.com>
2023-10-27 20:09:29 -0400

    Created skeleton for 3 modules for yatzy game

---
+++ YatzyEngine.js
@@ -0,0 +1,14 @@
+class YatzyEngine {
+
+    calculateScore (category, diceValues){
+        //returns the score for the given category based on the dice values passed as an array
+    }
+
+    isValidSelection (category, diceValues){
+        //checks whether the current dice are valid for the category the player is trying to score in
+    }
+
+    updateScore (player){
+        //this method updates the current player's score
+    }
+}
---
+++ YatzyGame.js
@@ -0,0 +1,23 @@
+class YatzyGame {
+    constructor() {
+        this.players = []; // An array to store the players
+        this.currentPlayer = 0; // Index of the current player
+        this.currentRound = 1; // Current round
+        this.totalScore = 0; // Total score of all players
+        this.winner = null; // Stores the winner at the end of the game
+    }
+
+    startNewGame(players) {
+        // Function to start a new game with a given number of players
+    }
+
+    // Method to end the current player's turn
+    endTurn(score) {
+        // Function to end the current player's turn
+    }
+
+    // Function to determine the winner and end the game
+    endGame() {
+        // Function to determine the winner and end the game
+    }
+}
